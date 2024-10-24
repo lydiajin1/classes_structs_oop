4835ceb1ef6e3a323a90c8e47c1704d4e6f31816
lydiajin1 <lydiajinr5@gmail.com>
2024-10-24 08:56:58 -0400

    experimented with classes, structs, oop.
    created the dude, the adventurer, and the main program files

---
+++ classes_structs_oop/classes_structs_oop/Dude.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_structs_oop
+{
+    internal class Dude
+    {
+        public string name;
+        public int experience = 0;
+
+        public Dude(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void PrintStatsInfo()
+        {
+            Console.WriteLine("Adventurer: " + this.name + " - " + this.experience + " EXP");
+        }
+
+        private void Reset()
+        {
+            this.name = "Not Assigned";
+            this.experience = 0;
+        }
+    }
+}
